a24879f6802f3d6f0eed1e8fb912497b204c82e1
Donny Davis <donnywdavis@icloud.com>
2016-04-28 11:05:44 -0400

    Set up our window and a root view controller inside a navigation controller

--- MuttCutsHitsTheRoad/MuttCutsHitsTheRoad/AppDelegate.m
+++ MuttCutsHitsTheRoad/MuttCutsHitsTheRoad/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,16 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    ViewController *viewController = [[ViewController alloc] init];
+    
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
+    
+    self.window.rootViewController = navigationController;
+    
+    [self.window makeKeyAndVisible];
+    
     // Override point for customization after application launch.
     return YES;
 }
