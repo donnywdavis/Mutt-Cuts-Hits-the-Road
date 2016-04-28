09768c995084fcf2c3057e20c57d9c806f733cce
Donny Davis <donnywdavis@icloud.com>
2016-04-28 11:32:26 -0400

    Create a map view and ask for authorization for location

--- MuttCutsHitsTheRoad/MuttCutsHitsTheRoad/ViewController.m
+++ MuttCutsHitsTheRoad/MuttCutsHitsTheRoad/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "Location.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+
+@property (strong, nonatomic) MKMapView *mapView;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -16,7 +21,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Set up the frame constraints for our view
+    CGRect theFrame = self.view.frame;
+    theFrame.origin.x = 0;
+    theFrame.origin.y = 64;
+    theFrame.size.height -= 64;
+    theFrame.size.width -= 0;
+    
+    // Let's get authorization to use the users location
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    [self.locationManager requestAlwaysAuthorization];
+//    [self.locationManager startUpdatingLocation];
+    
+    // Set up the map view
+    self.mapView = [[MKMapView alloc] initWithFrame:theFrame];
+    
+    self.mapView.showsUserLocation = YES;
+    
+    // Add the map view to our main view
+    [self.view addSubview:self.mapView];
 }
 
 - (void)didReceiveMemoryWarning {
